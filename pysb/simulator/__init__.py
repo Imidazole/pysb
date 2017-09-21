cc8ac98a01c14e234eabc102ff2e502b29cf1d15
Alex Lubbock <code@alexlubbock.com>
2017-09-20 23:49:21 -0500

    Weave/cython support for PyDas integration

--- pysb/simulator/__init__.py
+++ pysb/simulator/__init__.py
@@ -2,7 +2,9 @@ from .base import SimulatorException, SimulationResult
 from .scipyode import ScipyOdeSimulator
 from .cupsoda import CupSodaSimulator
 from .stochkit import StochKitSimulator
+from .dae import DaeSimulator
 from .bng import BngSimulator, PopulationMap
 
 __all__ = ['BngSimulator', 'CupSodaSimulator', 'ScipyOdeSimulator',
-           'StochKitSimulator', 'SimulationResult', 'PopulationMap']
+           'StochKitSimulator', 'DaeSimulator',
+           'SimulationResult', 'PopulationMap']
